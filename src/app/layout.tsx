1dd36ef8b820f030a89ab723f6fb08bc6eb655c5
Sina Shateri <76934868+SinaShateri@users.noreply.github.com>
2023-06-15 20:59:23 +0330

    [fix]: font error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import Header from '@/common/components/header';
 import '@/common/styles/globals.css';
 import { Inter } from 'next/font/google';
 
-const inter = Inter();
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'ITech',
